0299de3d6b6262e277fc3b3d707d1d67363d2e73
Dan <cartond93@gmail.com>
2019-09-26 23:55:07 -0400

    Update's to app configuration and index

--- app.js
+++ app.js
@@ -18,3 +18,24 @@ app.get('/', (req, res) => {
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/node-blog");
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+var postSchema = new mongoose.Schema({ body: String });
+var Post = mongoose.model('Post', postSchema);
+
+app.post('/addpost', (req, res) => {
+  var postData = new Post(req.body);
+  postData.save().then( result => {
+    res.redirect('/');
+  }).catch(err => {
+    res.status(400).send("Unable to save data");
+  });
+});
+
+app.get("/", (req, res) => {
+  Post.find({}, (err, posts) => {
+    res.render('index', { posts: posts})
+  });
+});
