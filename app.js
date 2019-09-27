1e94199e86a3e46653715799c9cc301be0e5a0e7
Dan <cartond93@gmail.com>
2019-09-26 23:56:43 -0400

    Move listen

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 var express = require('express');
 var app = express();
 
-// Listen
-app.listen(3333, () => {
-  console.log('Server listening on 3333');
-});
-
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -38,4 +33,9 @@ app.get("/", (req, res) => {
   Post.find({}, (err, posts) => {
     res.render('index', { posts: posts})
   });
+});
+
+// Listen
+app.listen(3333, () => {
+  console.log('Server listening on 3333');
 });
